29be3bff6945824c10506868f3eb790669e92540
sujeongHeo401 <gotnwjd40@gmail.com>
2021-06-20 13:32:42 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import QuoteBox from "./components/QuoteBox";
+import "font-awesome/css/font-awesome.min.css";
+import "./assets/styles/global.scss";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    document.title = "随机 名言 生成器"
+  }, []);
+  return <QuoteBox />;
 }
 
 export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,11 @@
+
+/**
+ * Get a random number.
+ *
+ * @param {number} min
+ * @param {number} max
+ */
+
+ export const random = (min, max) => {
+    return Math.random() * (max - min) + min;
+  };
